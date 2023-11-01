ef6a3a44baf0746cc35de9171855b1a5d0ab533a
Roger Lovera <rloverab@yahoo.es>
2023-11-01 17:21:37 -0400

    fix list recaudos

--- src/app/recaudos.ts
+++ src/app/recaudos.ts
@@ -2,6 +2,6 @@ import { Data } from "./data";
 
 export const RECAUDOS: Data[] = [
     { id: 1, value: 'Fotocopia del título universitario.', value2: '', value3: '' },
-    { id: 2, value: 'Fotocopia de la cédula de identida.', value2: '', value3: '' },
+    { id: 2, value: 'Fotocopia de la cédula de identidad.', value2: '', value3: '' },
     { id: 3, value: 'Una (1) foto de frente tamaño carnet (reciente) pegada en una de las planillas de inscripción.', value2: '', value3: '' }
 ];
